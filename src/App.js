f25c133439a207f835a69078ebc21883aa3ee238
IrynaHuk <132898409+IrynaHuk@users.noreply.github.com>
2023-12-04 11:02:47 +0000

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Iryna Huk and is{" "}
+        <a href="https://github.com/IrynaHuk/weather-react-app" target="_blanc">
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
