40d394e17f31d6a0261210811ab4c5956d325aa7
IrynaHuk <132898409+IrynaHuk@users.noreply.github.com>
2023-12-04 12:39:02 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
+      <button className="btn btn-primary"> test</button>
       <h1>Weather App</h1>
       <footer>
         This project was coded by Iryna Huk and is{" "}
         <a href="https://github.com/IrynaHuk/weather-react-app" target="_blanc">
-          open sourced on GitHub
+          open-sourced on GitHub
         </a>
       </footer>
     </div>
