423063d1dfe4ab1d36f1a50a0c72fd8bb804ed9e
IrynaHuk <132898409+IrynaHuk@users.noreply.github.com>
2023-12-11 14:45:14 +0000

    coded forecast for 5 days

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherForecastDay(props) {
+  let [pic, setPic] = useState("");
+  function showPic() {
+    setPic(
+      `https://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`
+    );
+  }
+  console.log(props.data.weather[0].icon);
   function maxTemperature() {
     let temperature = Math.round(props.data.temp.max);
     return `${temperature}°`;
@@ -19,8 +26,10 @@ export default function WeatherForecastDay(props) {
 
   return (
     <div>
-      <div className="WeatherForecast-day">{day}</div>
-      <div>{`https://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`}</div>
+      <div className="WeatherForecast-day">{day()}</div>
+      <div>
+        <img src={pic} />
+      </div>
 
       <div className="WeatherForecast-temperatures">
         <span className="WetherForecast-temperature-max">
