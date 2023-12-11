d56c75ae71a26aa1ca97fc07dd9e597e762cbeb1
IrynaHuk <132898409+IrynaHuk@users.noreply.github.com>
2023-12-11 14:59:12 +0000

    add netlify link

--- src/App.js
+++ src/App.js
@@ -7,8 +7,20 @@ export default function App() {
       <Weather defaultCity="New York" />
       <footer>
         This project was coded by Iryna Huk and is{" "}
-        <a href="https://github.com/IrynaHuk/weather-react-app" target="_blanc">
+        <a
+          href="https://github.com/IrynaHuk/weather-react-app"
+          target="_blanc"
+          rel="noopener noreferrer"
+        >
           open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://extraordinary-gingersnap-976c36.netlify.app/"
+          target="_blanc"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
         </a>
       </footer>
     </div>
